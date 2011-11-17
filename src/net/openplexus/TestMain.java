d1d77e9e22108a650466fb27f179e109163216b7
Robert Giacinto <>
2011-11-17 12:19:37 +0100

    kleinere Änderungen

--- src/net/openplexus/TestMain.java
+++ src/net/openplexus/TestMain.java
@@ -5,23 +5,35 @@
 package net.openplexus;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Robert Giacinto
  */
 public class TestMain {
-
+    
     public static void main(String[] args) {
         File f = new File("docs/inf/ti_bachelor_modulhandbuch.pdf");
         String text = TextStripper.extract(f);
         ModuleExtractor miExtractor = new ModuleExtractor(text, ModuleExtractor.TYPE_CS);
+        
+        f = new File("docs/inf/wi_bachelor_modulhandbuch.pdf");
+        text = TextStripper.extract(f);
+        ModuleExtractor wiExtractor = new ModuleExtractor(text, ModuleExtractor.TYPE_CS);
+        
+        
         Vocabulary vocab = new Vocabulary();
-
-        for (Module m : miExtractor.getModules()) {
+        List<Module> modules = new ArrayList<Module>();
+        modules.addAll(miExtractor.getModules());
+        modules.addAll(wiExtractor.getModules());
+        
+        for (Module m : modules) {
             vocab.addTerms(m);
         }
-
+        
+        
         vocab.print();
     }
 }
